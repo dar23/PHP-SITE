0a2acc7d831e089ce4c900e62ccd3b1be31f127f
dar23 <dariuszhozer23@gmail.com>
2023-07-10 21:30:21 +0200

    now

--- js/hidden_scroll.js
+++ js/hidden_scroll.js
@@ -5,7 +5,7 @@ let scroll_over=document.querySelector('.entries_post');
 scroll_over.addEventListener('mouseover', ()=>{
 
 scroll_over.style.overflowY="scroll";
-scroll_over.style.width="104%";
+scroll_over.style.width="100%";
 
 
 });
@@ -14,8 +14,8 @@ scroll_over.style.width="104%";
 
 scroll_over.addEventListener('mouseleave', ()=>{
 
-    scroll_over.style.overflowY="hidden";
-    scroll_over.style.width="100.5%";
+    scroll_over.style.overflowY="scroll";
+    scroll_over.style.width="100%";
     
     
     });
