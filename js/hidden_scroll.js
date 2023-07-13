ea9d208a9b290c2755f2987626f1cbfe7fc7820b
dar23 <dariuszhozer23@gmail.com>
2023-07-13 15:53:41 +0200

    now

--- js/hidden_scroll.js
+++ js/hidden_scroll.js
@@ -1,58 +1,25 @@
-const html_class=document.querySelector('.html_class'); //  cała strona
-const record_video=document.querySelector('.records_video'); // kontener wewnątrz strony
+const html_class = document.querySelector('.html_class'); // cała strona
+const record_video = document.querySelector('.records_video'); // kontener wewnątrz strony
 
-
-
-
-
-
-const record_video_offsetTop = record_video.offsetTop; //górny brzeg div record_video;
-const page_height= window.innerHeight; //wysokość okna przeglądarki; 
-const record_video_height=record_video.offsetHeight; //wysokość kontenera record_video;
-
-
-
-
+const record_video_offsetTop = record_video.offsetTop; // górny brzeg div record_video;
+const page_height = window.innerHeight; // wysokość okna przeglądarki; 
+const record_video_height = record_video.offsetHeight; // wysokość kontenera record_video
 
 const scrollEnd = record_video_offsetTop + record_video_height - page_height;
 
-
-
-function stopscroll(){
-
-    if(window.scrollY>=scrollEnd){
-
-   
-     
-  html_class.style.overflowY="hidden";
-
-
-    }
-
-   
-
-    
+function stopscroll() {
+  if (window.scrollY >= scrollEnd) {
+    html_class.style.overflowY = "hidden";
+  }
 }
 
 record_video.addEventListener('mouseover', stopscroll);
 
-
-
-
-function endscroll(){
-
- 
-html_class.style.overflowY="visible";
-
-
-  
+function endscroll() {
+  html_class.style.overflowY = "visible";
 }
 
-
 record_video.addEventListener('mouseleave', endscroll);
-
-
-
 
 
 
