5f07b197eaf9fe71e736cb288a6af5a54d1781f1
dar23 <dariuszhozer23@gmail.com>
2023-12-07 11:17:01 +0100

    new

--- menu.php
+++ menu.php
@@ -2,11 +2,14 @@
  
 <ul>
 
-    <li><a>O mnie</a></li>
 
-    <li><a>Kontakt</a></li>
 
-    <li class="button_admin"><a>Kierownik</a></li>
+  
+
+    <li class="button_admin"><a>DODAJ FILMIK</a></li>
+    <li class="button_admin"><a>DODAJ MEMA</a></li>
+   
+
 
 </ul>
 
